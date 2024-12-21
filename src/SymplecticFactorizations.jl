ab0adace8814c9e7cc1fa87cd0fc5b73415c2580
apkille <killeandrew@gmail.com>
2024-12-21 18:56:14 -0500

    add takagi decomp

--- src/SymplecticFactorizations.jl
+++ src/SymplecticFactorizations.jl
@@ -1,17 +1,20 @@
 module SymplecticFactorizations
 
 import LinearAlgebra
-using LinearAlgebra: mul!, Diagonal, qr, Factorization, svd, require_one_based_indexing
+using LinearAlgebra: mul!, Diagonal, qr, Factorization, svd, require_one_based_indexing, Symmetric, eigen
 
 export 
     # symplectic stuff
     issymplectic, symplecticform, BlockForm, PairForm, randsymplectic,
     # polar decomposition
-    polar, Polar
-
+    polar, Polar,
+    # takagi/autonne decomposition
+    takagi, Takagi
 
 include("symplectic.jl")
 
 include("polar.jl")
+
+include("takagi.jl")
 
 end
