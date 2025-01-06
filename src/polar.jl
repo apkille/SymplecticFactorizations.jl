6dee0df54b6026cb731cd25912b15cc04d9754c3
apkille <killeandrew@gmail.com>
2025-01-06 15:21:28 -0500

    fix polar decomp ordering

--- src/polar.jl
+++ src/polar.jl
@@ -1,31 +1,31 @@
 struct Polar{T,M<:AbstractArray{T},N<:AbstractArray{T}} <: Factorization{T}
-    P::M
-    O::N
-    function Polar{T,M,N}(P, O) where {T,M<:AbstractArray{T},N<:AbstractArray{T}}
-        require_one_based_indexing(P, O)
-        new{T,M,N}(P, O)
+    O::M
+    P::N
+    function Polar{T,M,N}(O, P) where {T,M<:AbstractArray{T},N<:AbstractArray{T}}
+        require_one_based_indexing(O, P)
+        new{T,M,N}(O, P)
     end
 end
-Polar{T}(P::AbstractArray{T}, O::AbstractArray{T}) where {T} = Polar{T,typeof(P),typeof(O)}(P,O)
+Polar{T}(O::AbstractArray{T}, P::AbstractArray{T}) where {T} = Polar{T,typeof(O),typeof(P)}(O,P)
 
 # iteration for destructuring into components
-Base.iterate(F::Polar) = (F.P, Val(:O))
-Base.iterate(F::Polar, ::Val{:O}) = (F.O, Val(:done))
+Base.iterate(F::Polar) = (F.O, Val(:P))
+Base.iterate(F::Polar, ::Val{:P}) = (F.P, Val(:done))
 Base.iterate(F::Polar, ::Val{:done}) = nothing
 
 function polar(x::AbstractMatrix{T}) where {T}
     fact = svd(x)
     dims = size(x)
-    P, O = zeros(T, dims), zeros(T, dims)
-    copyto!(P, fact.V * Diagonal(fact.S) * fact.Vt)
+    O, P = zeros(T, dims), zeros(T, dims)
     mul!(O, fact.U, fact.Vt)
-    return Polar{T}(P, O)
+    copyto!(P, fact.V * Diagonal(fact.S) * fact.Vt)
+    return Polar{T}(O, P)
 end
 
 function Base.show(io::IO, mime::MIME{Symbol("text/plain")}, F::Polar{<:Any,<:AbstractArray,<:AbstractArray})
     Base.summary(io, F); println(io)
-    println(io, "P factor:")
-    Base.show(io, mime, F.P)
-    println(io, "\nO factor:")
+    println(io, "O factor:")
     Base.show(io, mime, F.O)
+    println(io, "\nP factor:")
+    Base.show(io, mime, F.P)
 end
--- test/test_polar.jl
+++ test/test_polar.jl
@@ -11,16 +11,16 @@
         S_pair = randsymplectic(Omega)
 
         F_block = polar(S_block)
-        P_block, O_block = polar(S_block)
-        @test F_block.P == P_block && F_block.O == O_block
+        O_block, P_block = polar(S_block)
+        @test F_block.O == O_block && F_block.P == P_block
         F_pair = polar(S_pair)
-        P_pair, O_pair = polar(S_pair)
-        @test F_pair.P == P_pair && F_pair.O == O_pair
-        @test issymplectic(J, P_block, atol = 1e-5) && issymplectic(J, O_block, atol = 1e-5)
-        @test issymplectic(Omega, P_pair, atol = 1e-5) && issymplectic(Omega, O_pair, atol = 1e-5)
+        O_pair, P_pair = polar(S_pair)
+        @test F_pair.O == O_pair && F_pair.P == P_pair 
+        @test issymplectic(J, O_block, atol = 1e-5) && issymplectic(J, P_block, atol = 1e-5) 
+        @test issymplectic(Omega, O_pair, atol = 1e-5) && issymplectic(Omega, P_pair, atol = 1e-5)
+        @test isapprox(inv(O_pair), transpose(O_pair), atol = 1e-5) && isapprox(inv(O_block), transpose(O_block), atol = 1e-5)
         @test isapprox(P_block, transpose(P_block), atol = 1e-5) && all(i > 0 for i in eigvals(P_block))
         @test isapprox(P_pair, transpose(P_pair), atol = 1e-5) && all(i > 0 for i in eigvals(P_pair))
-        @test isapprox(inv(O_pair), transpose(O_pair), atol = 1e-5) && isapprox(inv(O_block), transpose(O_block), atol = 1e-5)
-        
+        @test isapprox(O_block * P_block, S_block, atol = 1e-5) && isapprox(O_pair * P_pair, S_pair, atol = 1e-5)
     end
 end
