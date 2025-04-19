4a33e2dff12e9ac9ebfd12c95a9a3e9b879ebc59
apkille <killeandrew@gmail.com>
2025-04-19 11:46:17 -0400

    add more testcov

--- test/test_givens.jl
+++ test/test_givens.jl
@@ -25,16 +25,27 @@
         @test M_block == Matrix(G_block) && M_pair == Matrix(G_pair)
         @test M_block == Array(G_block) && M_pair == Array(G_pair)
         @test AbstractMatrix{Float32}(M_block) == AbstractMatrix{Float32}(G_block) && AbstractMatrix{Float32}(M_pair) == AbstractMatrix{Float32}(G_pair)
-        @test G_block[1,1] == 1.0 && G_block[3,3] == 1.0 && G_block[4,4] == 1.0
-        @test G_block[5,5] == 1.0 && G_block[7,7] == 1.0 && G_block[8,8] == 1.0
-        @test G_block[2,2] == c   && G_block[6,6] == c
-        @test G_block[6,2] == -s  && G_block[2,6] == s
-        @test G_block[1,2] == 0.0 && G_block[2,1] == 0.0 && G_block[7,8] == 0.0 && G_block[8,7] == 0.0
-        @test  G_pair[1,1] == 1.0 &&  G_pair[2,2] == 1.0 &&  G_pair[5,5] == 1.0
-        @test  G_pair[6,6] == 1.0 &&  G_pair[7,7] == 1.0 &&  G_pair[8,8] == 1.0
-        @test  G_pair[3,3] == c   &&  G_pair[4,4] == c
-        @test  G_pair[4,3] == -s  &&  G_pair[3,4] == s
-        @test  G_pair[1,2] == 0.0 &&  G_pair[2,1] == 0.0 &&  G_pair[7,8] == 0.0  && G_pair[8,7] == 0.0
+        @test G_block[1,1] == G_block[3,3] == G_block[4,4] == 1.0
+        @test G_block[5,5] == G_block[7,7] == G_block[8,8] == 1.0
+        @test G_block[2,2] == G_block[6,6] == c
+        @test G_block[6,2] == -s && G_block[2,6] == s
+        @test G_block[1,2] == G_block[2,1] == G_block[7,8] == G_block[8,7] == 0.0
+        @test  G_pair[1,1] ==  G_pair[2,2] ==  G_pair[5,5] == 1.0
+        @test  G_pair[6,6] ==  G_pair[7,7] ==  G_pair[8,8] == 1.0
+        @test  G_pair[3,3] ==  G_pair[4,4] == c
+        @test  G_pair[4,3] == -s && G_pair[3,4] == s
+        @test  G_pair[1,2] ==  G_pair[2,1] ==  G_pair[7,8] == G_pair[8,7] == 0.0
+        @test  G_block[1]  ==  G_block[19] ==  G_block[28] == 1.0
+        @test  G_block[37] ==  G_block[55] ==  G_block[64] == 1.0
+        @test  G_block[10] ==  G_block[46] == c
+        @test  G_block[14] == -s &&  G_block[42] == s
+        @test  G_block[2]  ==   G_block[9] ==  G_block[56] == G_block[63] == 0.0
+        @test   G_pair[1]  ==   G_pair[10] ==  G_pair[37] == 1.0
+        @test   G_pair[46] ==   G_pair[55] ==  G_pair[64] == 1.0
+        @test   G_pair[19] ==   G_pair[28] == c
+        @test   G_pair[20] == -s &&  G_pair[27] == s
+        @test   G_pair[2]  ==    G_pair[9] ==  G_pair[56] ==   G_pair[63] == 0.0
+        
     end
     @testset "base methods" begin
         n = rand(3:5)
@@ -45,6 +56,7 @@
 
         @inbounds for form in [J, Omega]
             G = givens(form, k, θ)
+            @test isequal(G, copy(G)) && isapprox(G, copy(G))
             @test SymplecticGivens(G) == G
             @test copy(G) == G
             @test size(G) == (2n, 2n) && size(G, 1) == size(G, 2) == 2n
